d9753bbc829e5812985cd339e0dfddfb5505a106
Luisa <luisa.santos042@gmail.com>
2023-01-18 19:24:31 -0300

    setup prisma schema

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 const server = Fastify();
+const prisma = new PrismaClient();
 
 server.get('/', (req, res) => {
   return res.redirect('/habits');
@@ -10,6 +12,10 @@ server.get('/habits', () => {
   return 'habits list';
 });
 
-server.listen({
-  port: 3333,
-});
+server
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('server running on port 3333');
+  });
