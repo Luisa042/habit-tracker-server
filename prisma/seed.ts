ed18f1ccb74432223a5a254472ff6f45ac54fb1b
Luisa <luisa.santos042@gmail.com>
2023-01-22 13:30:39 -0300

    change seed.ts, adding days

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,10 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+const habit1 = '16c89f09-0ada-4203-aefd-3eec2e383277';
+const habit2 = '46b775ea-4d08-44e3-8c6e-17d73ee08c7e';
+const habit3 = 'c9305d0b-b125-4270-9735-ef2c48f78112';
+
 async function seed() {
   await prisma.habit.deleteMany();
   await prisma.day.deleteMany();
@@ -9,7 +13,7 @@ async function seed() {
   await Promise.all([
     prisma.habit.create({
       data: {
-        id: '16c89f09-0ada-4203-aefd-3eec2e383277',
+        id: habit1,
         title: 'sleep before midnight',
         createdAt: new Date('2023-01-22T00:00:00.000z'),
         WeekDays: {
@@ -26,7 +30,7 @@ async function seed() {
 
     prisma.habit.create({
       data: {
-        id: '46b775ea-4d08-44e3-8c6e-17d73ee08c7e',
+        id: habit2,
         title: 'drink 2l of water',
         createdAt: new Date('2023-01-23T00:00:00.000z'),
         WeekDays: {
@@ -45,11 +49,46 @@ async function seed() {
 
     prisma.habit.create({
       data: {
-        id: 'c9305d0b-b125-4270-9735-ef2c48f78112',
+        id: habit3,
         title: 'exercise',
         createdAt: new Date('2023-01-22T00:00:00.000z'),
         WeekDays: {
           create: [{ weekDay: 1 }, { weekDay: 3 }, { weekDay: 5 }],
+        },
+      },
+    }),
+  ]);
+
+  await Promise.all([
+    prisma.day.create({
+      data: {
+        date: new Date('2023-01-24T08:00:00.000z'),
+        DayVsHabits: {
+          create: {
+            habitId: habit2,
+          },
+        },
+      },
+    }),
+
+    prisma.day.create({
+      data: {
+        date: new Date('2023-01-25T08:00:00.000z'),
+        DayVsHabits: {
+          create: [{ habitId: habit1 }, { habitId: habit3 }],
+        },
+      },
+    }),
+
+    prisma.day.create({
+      data: {
+        date: new Date('2023-01-26T08:00:00.000z'),
+        DayVsHabits: {
+          create: [
+            { habitId: habit1 },
+            { habitId: habit2 },
+            { habitId: habit3 },
+          ],
         },
       },
     }),
